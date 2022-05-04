4c202d0ec0b49c2092ea48ea847bd402c6016367
Jaideep <guntupallijaideep@gmail.com>
2022-05-05 02:42:01 +0530

    init commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Cric-o-pedia
+
+This is the code base of cric-o-pedia, an interactive platform to chat with other cricket fans and visualise cricket statistics.
+
+## Table of contents
+
+- [Cric-o-pedia](#cric-o-pedia)
+  - [Table of contents](#table-of-contents)
+  - [Overview](#overview)
+    - [What is the project?](#what-is-the-project)
+    - [What is the MVP (Minimal Viable Product)?](#what-is-the-mvp-minimal-viable-product)
+    - [What are the sprinkles?](#what-are-the-sprinkles)
+    - [When will the project be complete?](#when-will-the-project-be-complete)
+    - [Links](#links)
+  - [My process](#my-process)
+    - [Built with](#built-with)
+
+## Overview
+
+### What is the project?
+
+This is a project made as part of Designing Interactive Systems course.
+
+### What is the MVP (Minimal Viable Product)?
+
+The MVP is the function to chat with other cricket fans during live match as well as able to visualise different cricket statistics.
+
+### What are the sprinkles?
+
+The sprinkles would be adding different animations and able to visualise maximum types of cricket statistics
+
+### When will the project be complete?
+
+MVP should be done by 31 Apr 2022.
+
+### Links
+
+-   Website: [Link to Deployment](#)
+
+## My process
+
+### Built with
+
+-   Semantic HTML5 markup
+-   Tailwind CSS
+-   Flexbox
+-   Mobile-first workflow
+-   ReactJS
+-   NodeJS
+-   ExpressJS
---
+++ index.js
@@ -0,0 +1,121 @@
+const express = require("express");
+const axios = require("axios").default;
+const cors = require("cors");
+const { response } = require("express");
+
+const app = express();
+
+app.use(cors());
+
+const imgURL =
+    "https://img1.hscicdn.com/image/upload/f_auto,t_ds_wide_w_1200,q_60/lsci";
+
+const sqImgURL =
+    "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci";
+
+const storyURL = "https://www.espncricinfo.com/story/";
+
+app.get("/homeMatches", async (req, res) => {
+    axios({
+        url: "https://hs-consumer-api.espncricinfo.com/v1/pages/matches/current?lang=en",
+        method: "get",
+    })
+        .then((response) => {
+            const cusResponse = [];
+            const data = response.data.matches;
+            data.map((ele) => {
+                if (ele.series.alternateName === "IPL") {
+                    let newTeams = [];
+                    ele.teams.map((team) => {
+                        let newTeamImage = {
+                            url: sqImgURL + team.team.image.url,
+                            caption: team.team.image.caption,
+                        };
+                        let newTeam = {
+                            slug: team.team.slug,
+                            name: team.team.name,
+                            longName: team.team.longName,
+                            abbreviation: team.team.abbreviation,
+                            image: newTeamImage,
+                            score: team.score,
+                            scoreInfo: team.scoreInfo,
+                            inningNumbers: team.inningNumbers[0],
+                        };
+                        newTeams.push(newTeam);
+                    });
+                    let newJSON = {
+                        id: ele.objectId,
+                        slug: ele.slug,
+                        state: ele.state,
+                        startTime: ele.startTime,
+                        statusText: ele.statusText,
+                        teams: newTeams,
+                    };
+                    // console.log(ele);
+                    cusResponse.push(newJSON);
+                }
+            });
+            res.status(200).json(cusResponse);
+        })
+        .catch((err) => {
+            res.status(500).json({ message: err });
+        });
+});
+
+app.get("/news", async (req, res) => {
+    axios({
+        url: "https://hs-consumer-api.espncricinfo.com/v1/edition/feed?edition=in&lang=en&page=1&records=10",
+        method: "get",
+    })
+        .then((response) => {
+            const cusResponse = [];
+            const data = response.data.results;
+            data.map((ele) => {
+                ele.containers.map((news) => {
+                    if (
+                        news.enhanced &&
+                        news.type === "DEFAULT" &&
+                        news.item.type === "STORY"
+                    ) {
+                        let newJSON = {
+                            id: news.item.story.id,
+                            title: news.item.story.title,
+                            summary: news.item.story.summary,
+                            storyUrl:
+                                storyURL +
+                                news.item.story.slug +
+                                "-" +
+                                news.item.story.objectId,
+                            imgUrl:
+                                imgURL + news.item.story.image.peerUrls.WIDE,
+                            imgAlt: news.item.story.image.caption,
+                        };
+                        cusResponse.push(newJSON);
+                    }
+                });
+            });
+            res.status(200).json(cusResponse);
+        })
+        .catch((err) => {
+            res.status(500).json({ message: err });
+        });
+});
+
+app.get("/score", async (req, res) => {
+    const matchId = req.query.matchId;
+    const requrl = `https://hs-consumer-api.espncricinfo.com/v1/pages/match/details?lang=en&seriesId=1298423&matchId=${matchId}&latest=true`;
+    axios({
+        url: requrl,
+        method: "get",
+    })
+        .then((response) => {
+            res.status(200).json(response.data);
+        })
+        .catch((err) => {
+            res.status(500).json({ message: err });
+        });
+});
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log("Listening on port 8080");
+});
